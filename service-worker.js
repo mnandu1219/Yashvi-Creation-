ad37e72e415cff5f21a6a757ce619123c8355a68
mnandu1219 <mihir.nandu12@gmail.com>
2026-09-10 09:25:32 +0530

    Update cache version from v17 to v18

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "gst-billing-v17";
+const CACHE = "gst-billing-v18";
 const ASSETS = [
   "./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png",
 ];
