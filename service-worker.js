5b2822daf930e59e11843abf688f7cc696cb28cb
mnandu1219 <mihir.nandu12@gmail.com>
2026-09-23 15:32:01 +0530

    Update cache version to gst-billing-v28

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "gst-billing-v26";
+const CACHE = "gst-billing-v28";
 const ASSETS = [
   "./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png",
 ];
