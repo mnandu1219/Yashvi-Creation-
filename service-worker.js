4127964c5b318b215e94caced24e8e66cef16f73
mnandu1219 <mihir.nandu12@gmail.com>
2026-09-23 19:38:06 +0530

    Update cache version from v36 to v37

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "gst-billing-v36";
+const CACHE = "gst-billing-v37";
 const ASSETS = [
   "./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png",
 ];
