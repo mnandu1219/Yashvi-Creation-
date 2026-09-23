65536c69d8c3627bf0725542ed673715152976db
mnandu1219 <mihir.nandu12@gmail.com>
2026-09-23 20:10:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "gst-billing-v38";
+const CACHE = "gst-billing-v39";
 const ASSETS = [
   "./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png",
 ];
